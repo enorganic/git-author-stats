cf4f458b741895dd8e882db125006247d46064f9
David Belais <David.Belais@nike.com>
2024-04-30 15:48:50 -0700

    initial commit

---
+++ git_author_stats/__init__.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+from ._stats import (
+    iter_stats,
+    Frequency,
+    FrequencyUnit,
+    Stats,
+)
+
+
+__all__: Tuple[str, ...] = (
+    "iter_stats",
+    "Frequency",
+    "FrequencyUnit",
+    "Stats",
+)
